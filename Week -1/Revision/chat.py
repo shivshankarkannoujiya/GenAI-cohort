a3a83c2bd3ec5424dacfcacb6b9ca2ee70325f36
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-08-22 04:50:42 +0530

    Add .gitignore, chat.py, and requirements.txt files

---
+++ Week -1/Revision/chat.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+result = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": "hey there"
+        }
+    ]
+)
+
+print(result.choices[0].message.content)
